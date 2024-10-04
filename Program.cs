f3d274db131e0f52e5de1b08b4aed2beaa0dd506
CommCode <dennis.hartstock@web.de>
2024-10-04 04:48:46 +0200

    add task solution

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] myStrings = new string[2]
+{ "I like pizza. I like roast chicken. I like salad",
+"I like all three of the menu choices"
+};
+
+int periodLocation;
+
+foreach (var myString in myStrings)
+{
+    do
+    {
+        periodLocation = myString.IndexOf(".");
+
+        if (periodLocation != -1)
+        {
+            myString.Remove(periodLocation).Substring(periodLocation + 1).TrimStart();
+            Console.WriteLine("");
+        }
+
+    } while (periodLocation == -1);
+}
